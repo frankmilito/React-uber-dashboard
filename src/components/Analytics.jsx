016047b3655c43ed851813e2111c9147058e5e11
Franklin Obidike <Franklin@Obi-Franklin.local>
2022-08-21 02:00:19 +0100

    analytics

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -4,9 +4,79 @@ import {BsFillCalendar2WeekFill} from "react-icons/bs"
 import {IoStatsChart} from "react-icons/io5"
 import {BiGroup} from "react-icons/bi"
 import {FiActivity} from "react-icons/fi"
-
+import {cardStyle} from "./ReusableStyles"
 export default function Analytics() {
-  return <div>Analytics</div>
+  return (
+    <Section>
+      <div className="analytic">
+        <div className="content">
+          <h5>Spent this month</h5>
+          <h2>$675.4</h2>
+        </div>
+        <div className="logo">
+          <BsFillCalendar2WeekFill />
+        </div>
+      </div>
+      <div className="analytic">
+        <div className="logo">
+          <IoStatsChart />
+        </div>
+        <div className="content">
+          <h5>Earnings</h5>
+          <h2>$350.1</h2>
+        </div>
+      </div>
+      <div className="analytic">
+        <div className="logo">
+          <BiGroup />
+        </div>
+        <div className="content">
+          <h5>New Clients</h5>
+          <h2>$321</h2>
+        </div>
+      </div>
+      <div className="analytic">
+        <div className="content">
+          <h5>Activity</h5>
+          <h2>$540.50</h2>
+        </div>
+        <div className="logo">
+          <FiActivity />
+        </div>
+      </div>
+    </Section>
+  )
 }
 
-const Section = styled.section``
+const Section = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  .analytic {
+    ${cardStyle};
+    padding: 1rem;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    gap: 1rem;
+    transition: 0.4s ease-in-out;
+    svg {
+      color: white;
+    }
+    &:hover {
+      background-color: #ffc107;
+      color: black;
+    }
+    .logo {
+      background-color: black;
+      border-radius: 3rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 1.5rem;
+      svg {
+        font-size: 1.5rem;
+      }
+    }
+  }
+`
